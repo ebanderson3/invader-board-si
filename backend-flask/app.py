758ca14b1cf732d44ffa3daf4e1f2fb33bcff333
Elliot Anderson <ebanderson@mail.lipscomb.edu>
2024-12-14 22:39:09 -0600

    Testing api between frontend and backend

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return jsonify(message='Hello, World!!!!')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- frontend-svelte/src/routes/+page.svelte
+++ frontend-svelte/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to Svelte 5!!!</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { onMount } from 'svelte';
+
+  let message = 'Loading...';
+
+  onMount(async () => {
+    const response = await fetch('http://localhost:5001/');
+    const data = await response.json();
+    message = data.message;
+  });
+</script>
+
+<main>
+  <h1>{message}</h1>
+</main>
+
+
